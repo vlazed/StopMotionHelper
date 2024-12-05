e567b7fbe87f4ec0a7a95219362437426c6e976e
vlazed <dinoboy.valdez4@gmail.com>
2024-12-05 00:35:53 -0600

    Select entities from other views other than the player + Extend the SMH entity selection to a player's view entities. This allows the player to select entities when viewing from a camera. Supports Advanced Cameras

--- lua/smh/client/derma/world_clicker.lua
+++ lua/smh/client/derma/world_clicker.lua
@@ -1,6 +1,26 @@
 local BaseClass = baseclass.Get("EditablePanel")
 local PANEL = {}
 
+-- https://github.com/penolakushari/RagdollMover/blob/eefbda5c3b27e193b1c3e113b258f7a1d4334cad/lua/autorun/ragdollmover.lua#L72
+local function GetViewTrace()
+    local player = LocalPlayer()
+    local viewEntity = player:GetViewEntity()
+
+    local eyePos = player:EyePos()
+    if IsValid(viewEntity) and viewEntity ~= player then
+        eyePos = viewEntity:EyePos()
+        if viewEntity:GetClass() == "hl_camera" then -- adding support for Advanced Camera's view offset https://steamcommunity.com/sharedfiles/filedetails/?id=881605937&searchtext=advanced+camera
+			eyePos = viewEntity:LocalToWorld(viewEntity:GetViewOffset())
+		end
+    end
+
+    return {
+        start = eyePos,
+        endpos = eyePos + player:GetAimVector() * 32678,
+        filter = viewEntity
+    }
+end
+
 function PANEL:Init()
 
     self:SetWorldClicker(true)
@@ -26,7 +46,7 @@ function PANEL:SetVisible(visible)
 end
 
 function PANEL:Think()
-    local trace = util.TraceLine(util.GetPlayerTrace(LocalPlayer()))
+    local trace = util.TraceLine(GetViewTrace())
 
     self:OnEntityHovered(trace.HitNonWorld and trace.Entity)
 end
@@ -36,7 +56,7 @@ function PANEL:OnMousePressed(mousecode)
         return
     end
 
-    local trace = util.TraceLine(util.GetPlayerTrace(LocalPlayer()))
+    local trace = util.TraceLine(GetViewTrace())
     if not IsValid(trace.Entity) then return end
 
     local setting = 0
