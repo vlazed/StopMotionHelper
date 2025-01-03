f8054da6542b565c916ed8c2a72b63cbeff5916b
vlazed <dinoboy.valdez4@gmail.com>
2025-01-02 18:12:20 -0600

    Don't let SMH initialize the entity with advanced colours - Changed the behavior of saving and loading advanced colours. Users control when they add advanced colors. - If a certain color keyframe doesn't exist for interpolation, use the first, existing color

--- lua/smh/modifiers/advcolor.lua
+++ lua/smh/modifiers/advcolor.lua
@@ -5,15 +5,6 @@ function MOD:AdvColorInstalled(entity)
     return isfunction(entity.SetSubColor) and entity._adv_colours and next(entity._adv_colours)
 end
 
-function MOD:InitializeEntity(entity)
-    -- Initialize all advanced colour fields
-    for i = 0, #entity:GetMaterials() - 1 do
-        if not entity._adv_colours[i] then
-            entity:SetSubColor(i, color_white)
-        end
-    end
-end
-
 function MOD:Save(entity)
 
     if not self:AdvColorInstalled(entity) then return end
@@ -21,8 +12,6 @@ function MOD:Save(entity)
     if self:IsEffect(entity) then
         entity = entity.AttachedEntity;
     end
-
-    self:InitializeEntity(entity)
 
     local data = {}
     for i, color in pairs(entity._adv_colours) do
@@ -35,8 +24,6 @@ end
 function MOD:Load(entity, data)
 
     if not self:AdvColorInstalled(entity) then return end
-
-    self:InitializeEntity(entity)
 
     if self:IsEffect(entity) then
         entity = entity.AttachedEntity;
@@ -52,18 +39,17 @@ function MOD:LoadBetween(entity, data1, data2, percentage)
 
     if not self:AdvColorInstalled(entity) then return end
 
-    self:InitializeEntity(entity)
-
     if self:IsEffect(entity) then
         entity = entity.AttachedEntity;
     end
 
     for i, c1 in pairs(data1) do
-        local c2 = data2[i]
-        local r = SMH.LerpLinear(c1.r, c2.r, percentage);
-        local g = SMH.LerpLinear(c1.g, c2.g, percentage);
-        local b = SMH.LerpLinear(c1.b, c2.b, percentage);
-        local a = SMH.LerpLinear(c1.a, c2.a, percentage);
+        local c2 = data2[i] or c1
+        local diff = c1 ~= c2
+        local r = diff and SMH.LerpLinear(c1.r, c2.r, percentage) or c1.r;
+        local g = diff and SMH.LerpLinear(c1.g, c2.g, percentage) or c1.g;
+        local b = diff and SMH.LerpLinear(c1.b, c2.b, percentage) or c1.b;
+        local a = diff and SMH.LerpLinear(c1.a, c2.a, percentage) or c1.a;
 
         entity:SetSubColor(i, Color(r, g, b, a))
     end
