c333eff992849a138576a348152d9cc7e15d6945
vlazed <35244845+vlazed@users.noreply.github.com>
2026-04-11 10:39:34 -0400

    Add GBT modifiers from `gbt-modifiers`

--- lua/autorun/client/smh_version.lua
+++ lua/autorun/client/smh_version.lua
@@ -1,7 +1,7 @@
 ---Current ISO date since this has been versioned.
 ---May produce false positives, so I try to offset this by plus a minute or two to the future. Doesn't seem as reliable,
 ---but it's able to cover cases where either the user downloaded this addon with a zip, or `git clone`d it
-local DATE = "2026-04-11T11:38:28Z"
+local DATE = "2026-04-11T14:41:04Z"
 local changelog = ""
 
 local URL = "https://github.com/vlazed/StopMotionHelper/tree/develop"
